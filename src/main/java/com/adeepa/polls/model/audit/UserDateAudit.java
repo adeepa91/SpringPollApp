30fe59c6fd2086487cf9f4119b35baabb411f511
Adeepa91 <Adeepa91@Adeepa>
2018-07-02 00:27:54 +0530

    Create poll, add vote, view polls and votes, view profile

---
+++ src/main/java/com/adeepa/polls/model/audit/UserDateAudit.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adeepa.polls.model.audit;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+/**
+ *
+ * @author Adeepa91
+ */
+@MappedSuperclass
+@JsonIgnoreProperties(
+        value = {"createdBy", "updatedBy"},
+        allowGetters = true
+)
+public abstract class UserDateAudit extends DateAudit{
+    
+    @CreatedBy
+    @Column(updatable = false)
+    private Long createdBy;
+
+    @LastModifiedBy
+    private Long updatedBy;
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Long getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Long updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+}
